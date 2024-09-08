0ad7f91b50c3483c401633d975a84b4d7a8ce05f
Akshat <akshatp1865@gmail.com>
2024-09-09 03:17:43 +0530

    ContractUpdate

--- todo.sol
+++ todo.sol
@@ -12,16 +12,15 @@ contract Todo{
     // Task Struct
     struct Task{
         string content;
-        priority taskPriority;
         string category;
-        bool completed;
+        priority taskPriority;
         uint dueDate;
+        bool completed;
         bool deleted;
     }
 
     // Mappings
-    mapping(address => mapping(uint => Task)) private userTasks;
-    mapping(address => uint) private taskCount;
+    mapping(address => Task[]) private _userTasks;
 
     // Events
     event TaskAdded(address indexed user, uint256 taskId, string content, string category, priority taskPriority, uint dueDate);
@@ -32,68 +31,75 @@ contract Todo{
     event PriorityEdited(address indexed user, uint256 taskId, priority newPriority);
     event DueDateEdited(address indexed user, uint256 taskId, uint newDueDate);
 
-    modifier correctTaskId(address _user, uint256 _taskId) {
-        require(_taskId < taskCount[_user], "Invalid task ID");
+    modifier validTaskId( uint256 _taskId) {
+        require(_taskId < _userTasks[msg.sender].length, "Invalid task ID");
         _;
     }
 
     // Create Task
     function addTask(string memory _content, string memory _category, priority _taskPriority, uint _dueDate) public {
-        uint id = ++taskCount[msg.sender];
-        userTasks[msg.sender][id] = Task( _content, _taskPriority, _category,false, _dueDate, false);
-        emit TaskAdded(msg.sender, id, _content, _category, _taskPriority, _dueDate);
+        Task memory newTask;
+        newTask.content = _content;
+        newTask.category = _category;
+        newTask.taskPriority = _taskPriority;
+        newTask.dueDate = _dueDate;
+        newTask.completed = false;
+        newTask.deleted = false;
+
+        _userTasks[msg.sender].push(newTask);
+        emit TaskAdded(msg.sender, _userTasks[msg.sender].length - 1, _content, _category, _taskPriority, _dueDate);
     }
 
-    // Edit Task Completion Status 
-    function editCompleteTask(uint _taskId) public correctTaskId(msg.sender, _taskId) {
-        userTasks[msg.sender][_taskId].completed = !userTasks[msg.sender][_taskId].completed;
-        emit EditTaskComplete(msg.sender, _taskId, userTasks[msg.sender][_taskId].completed);
-    }
+    // Edit Task Completion Status
+    function toggleCompleteTask(uint _taskId) public validTaskId(_taskId) {
+        _userTasks[msg.sender][_taskId].completed = !_userTasks[msg.sender][_taskId].completed;
+        emit EditTaskComplete(msg.sender, _taskId, _userTasks[msg.sender][_taskId].completed);
+    } 
 
     // Delete Task
-    function removeTask(uint _taskId) public correctTaskId(msg.sender, _taskId) {
-        userTasks[msg.sender][_taskId].deleted = true;
+    function removeTask(uint _taskId) public validTaskId(_taskId) {
+        _userTasks[msg.sender][_taskId].deleted = true;
         emit TaskDeleteStatus(msg.sender, _taskId, true);
     }
 
     // Recover Deleted Task
-    function recoverTask(uint _taskId) public correctTaskId(msg.sender, _taskId) {
-        userTasks[msg.sender][_taskId].deleted = false;
+    function recoverTask(uint _taskId) public validTaskId( _taskId) {
+        _userTasks[msg.sender][_taskId].deleted = false;
         emit TaskDeleteStatus(msg.sender, _taskId, false);
     }
 
     // Change Content
-    function editContent(uint _taskId, string memory _newContent) public correctTaskId(msg.sender, _taskId) {
-        userTasks[msg.sender][_taskId].content = _newContent;
+    function editContent(uint _taskId, string memory _newContent) public validTaskId(_taskId) {
+        _userTasks[msg.sender][_taskId].content = _newContent;
         emit ContentEdited(msg.sender, _taskId, _newContent);
     }
 
     //Change Category
-    function editCategory(uint _taskId, string memory _newCategory) public correctTaskId(msg.sender, _taskId) {
-        userTasks[msg.sender][_taskId].category = _newCategory;
+    function editCategory(uint _taskId, string memory _newCategory) public validTaskId(_taskId) {
+        _userTasks[msg.sender][_taskId].category = _newCategory;
         emit CategoryEdited(msg.sender, _taskId, _newCategory);
     }
 
     // Change Priority
-    function editPriority(uint _taskId, priority _newPriority) public correctTaskId(msg.sender, _taskId) {
-        userTasks[msg.sender][_taskId].taskPriority = _newPriority;
+    function editPriority(uint _taskId, priority _newPriority) public validTaskId(_taskId) {
+        _userTasks[msg.sender][_taskId].taskPriority = _newPriority;
         emit PriorityEdited(msg.sender, _taskId, _newPriority);
     }
 
     // Change Date
-    function editDueDate(uint _taskId, uint _newDueDate) public correctTaskId(msg.sender, _taskId) {
-        userTasks[msg.sender][_taskId].dueDate = _newDueDate;
+    function editDueDate(uint _taskId, uint _newDueDate) public validTaskId( _taskId) {
+        _userTasks[msg.sender][_taskId].dueDate = _newDueDate;
         emit DueDateEdited(msg.sender, _taskId, _newDueDate);
     }
 
     // Sorting and Filtering tasks can be implemented using Javascript instead of Solidity
     function getTasks() public view returns (Task[] memory) {
-        uint taskCountForUser = taskCount[msg.sender];
+        uint taskCountForUser = _userTasks[msg.sender].length;
         uint nonDeletedTaskCount = 0;
 
         // Count non-deleted tasks
         for (uint i = 0; i < taskCountForUser; i++) {
-            if (!userTasks[msg.sender][i].deleted) {
+            if (!_userTasks[msg.sender][i].deleted) {
                 nonDeletedTaskCount++;
             }
         }
@@ -103,8 +109,8 @@ contract Todo{
 
         // Add and return tasks to the array
         for (uint i = 0; i < taskCountForUser; i++) {
-            if (!userTasks[msg.sender][i].deleted) {
-                tasks[index] = userTasks[msg.sender][i];
+            if (!_userTasks[msg.sender][i].deleted) {
+                tasks[index] = _userTasks[msg.sender][i];
                 index++;
             }
         }
@@ -113,12 +119,12 @@ contract Todo{
     }
 
     function getDeletedTasks() public view returns (Task[] memory) {
-        uint taskCountForUser = taskCount[msg.sender];
+        uint taskCountForUser = _userTasks[msg.sender].length;
         uint deletedTaskCount = 0;
 
         // Count deleted tasks
         for (uint i = 0; i < taskCountForUser; i++) {
-            if (userTasks[msg.sender][i].deleted) {
+            if (_userTasks[msg.sender][i].deleted) {
                 deletedTaskCount++;
             }
         }
@@ -128,13 +134,12 @@ contract Todo{
 
         // Add and return deleted tasks to the array
         for (uint i = 0; i < taskCountForUser; i++) {
-            if (userTasks[msg.sender][i].deleted) {
-                tasks[index] = userTasks[msg.sender][i];
+            if (_userTasks[msg.sender][i].deleted) {
+                tasks[index] = _userTasks[msg.sender][i];
                 index++;
             }
         }
         return tasks;
     }
-
 
 }
